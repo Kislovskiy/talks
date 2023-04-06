db3ea04d485fe8e75fe71c61478f2a0d9d7f5c3f
kislovskiy <kislovskiy@protonmail.com>
2023-04-06 17:41:51 +0200

    prettify sine and cosine

--- 2023_PyData_Berlin/src/plot_cos.py
+++ 2023_PyData_Berlin/src/plot_cos.py
@@ -8,9 +8,26 @@ def plot_cos():
     plt.xkcd()
     fig, ax = plt.subplots()
     fnc = "cos"
-    x = np.linspace(0, 10, 100)
+    x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
     ax.plot(x, getattr(np, fnc)(x))
     ax.set_title(f"{fnc}")
+
+    ax.plot(x, getattr(np, fnc)(x))
+
+    ax.set_yticks([-1, 1])
+    ax.set_yticklabels(["-1", "+1"])
+
+    ax.set_xticks([-np.pi, -np.pi / 2, np.pi / 2, np.pi])
+    ax.set_xticklabels(["π-", "π-/2", "π+/2", "π+"])
+
+    ax.spines[["bottom", "left"]].set_position(("data", 0))
+    ax.spines[["top", "right"]].set_visible(False)
+
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False, zorder=10)
+    ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False, zorder=10)
+
+    ax.set_title(f"{fnc}")
+
     return fig
 
 
--- 2023_PyData_Berlin/src/plot_sin.py
+++ 2023_PyData_Berlin/src/plot_sin.py
@@ -8,9 +8,23 @@ def plot_sin():
     plt.xkcd()
     fig, ax = plt.subplots()
     fnc = "sin"
-    x = np.linspace(0, 10, 100)
+    x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
     ax.plot(x, getattr(np, fnc)(x))
+
+    ax.set_yticks([-1, 1])
+    ax.set_yticklabels(["-1", "+1"])
+
+    ax.set_xticks([-np.pi, -np.pi / 2, np.pi / 2, np.pi])
+    ax.set_xticklabels(["π-", "π-/2", "π+/2", "π+"])
+
     ax.set_title(f"{fnc}")
+
+    ax.spines[["bottom", "left"]].set_position(("data", 0))
+    ax.spines[["top", "right"]].set_visible(False)
+
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), clip_on=False, zorder=10)
+    ax.plot(0, 1, "^k", transform=ax.get_xaxis_transform(), clip_on=False, zorder=10)
+    
     return fig
 
 
